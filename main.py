199cf92b823058ef02a9a87ad875ea68e0ab9e40
Roheemat Adebiyi <ra667@njit.edu>
2024-10-29 17:14:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -66,3 +66,10 @@ def repl():
             print(f"Result: {result}")
         except Exception as e:
             print(f"Error: {e}")
+
+# main.py
+from app import App    
+
+# You must put this in your main.py because this forces the program to start when you run it from the command line.
+if __name__ == "__main__":
+    app = App().start()  # Instantiate an instance of App
